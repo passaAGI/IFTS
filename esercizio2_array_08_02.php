ca3f12ec1ef67a3ffb17b0c722a0dad134bfa535
Vincenzo Passariello <passa97@gmail.com>
2022-02-10 14:04:28 +0100

    Update esercizio2_array_08_02.php

--- esercizio2_array_08_02.php
+++ esercizio2_array_08_02.php
@@ -10,14 +10,24 @@
 	function build_table($array){
         $html = '<table>';
         $html .= '<tr>';
-        foreach ($array[0] as $key=>$value) {
+        foreach ($array[0] as $key=>$value){
                 $html .= '<th>' . htmlspecialchars($key) . '</th>';
             }
         $html .= '</tr>';
 
-        foreach ($array as $key=>$value) {
-            $html .= '<tr>';
-            foreach ($value as $key2=>$value2) {
+        $flag = 0;
+        foreach($array as $key=>$value){
+            switch($flag){
+                case 0:
+                    $html .= '<tr>';
+                    $flag += 1;
+                    break;
+                case 1:
+                    $html .= "<tr style='background-color:red'>";
+                    $flag -= 1;
+                    break;
+            }
+            foreach($value as $key2=>$value2){
                 $html .= '<td>' . htmlspecialchars($value2) . '</td>';
             }
             $html .= '</tr>';
@@ -27,5 +37,19 @@
         return $html;
     }       
     echo build_table($schedina);
-
+    //2.6
+    function count_draws($array){
+        $counter = 0;
+        for($i = 0; $i < count($array); $i++){
+            if($array[$i]["risultato"] == "X"){
+                $counter++;
+            }
+        }
+        return $counter;
+    }
+    echo "<hr>Numero di pareggi: " . count_draws($schedina) . "<hr>";
+    //2.7
+    for($i = 0; $i < count($schedina); $i++){
+        
+    }
 ?>
