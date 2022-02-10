f10a5a7e1292a006eefecf5b5e4554f1f2c4a1b8
Vincenzo Passariello <passa97@gmail.com>
2022-02-10 15:00:51 +0100

    Update esercizio2_array_08_02.php

--- esercizio2_array_08_02.php
+++ esercizio2_array_08_02.php
@@ -2,6 +2,7 @@
     /**
      * https://classroom.google.com/c/NDUyMDM2NjY0MTgx/a/NDYwNzg1MzgwNzIz/details
      */
+    $partita = ['squadra1'=>"", 'squadra2'=>"", 'risultato'=>""];
     $partita1 = ['squadra1' => "Inter", 'squadra2' => "Bologna", 'risultato' => '1'];
     $partita2 = ['squadra1' => "Juventus", 'squadra2' => "Inter", 'risultato' => 'X'];
     $partita3 = ['squadra1' => "Torino", 'squadra2' => "Sampdoria", 'risultato' => '2'];
@@ -49,7 +50,25 @@
     }
     echo "<hr>Numero di pareggi: " . count_draws($schedina) . "<hr>";
     //2.7
+    $last_sunday = date('d/m/Y', strtotime('last sunday'));
     for($i = 0; $i < count($schedina); $i++){
-        
+        $schedina[$i]["domenica_prec"] = $last_sunday;
     }
+    //2.8
+    $mia_schedina = [];
+    $possible_value = ["1", "2", "X"];
+    for($i = 0; $i < count($schedina); $i++){
+        $partita["risultato"] = $possible_value[rand(0, 2)];
+        array_push($mia_schedina, $partita);
+    }
+
+    $is_match = 0;
+    for($i = 0; $i < count($mia_schedina); $i++){
+        if($mia_schedina[$i]["risultato"] == $schedina[$i]["risultato"]){
+            $is_match += 1;
+        }
+    }
+    echo "Punteggio: $is_match<hr>";
+    //2.9
+    echo build_table($mia_schedina);
 ?>
